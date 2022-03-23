883582896b291170ddff5cd13457985516891eb3
eunannana <thisiseunkim93@gmail.com>
2022-03-23 23:16:17 +0700

    connection test to database

--- Tugas2_20200140136_A/Program.cs
+++ Tugas2_20200140136_A/Program.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Tugas2_20200140136_A
 {
-    internal class Program
+    public class Program
     {
+        public void InsertData()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source =ANN\\ANNSANDRYN; database=KontrakTenantMall;Integrated Security = TRUE");
+                con.Open();
+
+                Console.WriteLine("Successfully connected to Database!");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oops, Failed connected to Database... " + e);
+                Console.ReadKey();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
         static void Main(string[] args)
         {
+            new Program().InsertData();
         }
     }
 }
